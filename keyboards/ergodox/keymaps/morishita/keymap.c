eaa43f1add9c8ca76e902016813e889d1723b7d5
GuiltyMorishita <morilliantblue@gmail.com>
2017-08-23 08:12:46 +0900

    tmuxのprefixをC-jに変更、DELをLCTLに変更

--- keyboards/ergodox/keymaps/morishita/keymap.c
+++ keyboards/ergodox/keymaps/morishita/keymap.c
@@ -39,13 +39,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [BASE] = KEYMAP(  // layer 0 : default
         // left hand
         KC_ESC,         KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   LCTL(KC_B),
-        KC_TAB,         KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   LCTL(KC_A),
+        KC_TAB,         KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   LCTL(KC_J),
         KC_LGUI,        KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
         KC_LSFT,        KC_Z,         KC_X,   KC_C,   KC_V,   KC_B,   LCTL(KC_X),
         MO(LAYER1),     MO(LAYER2),   KC_LALT,KC_LCTL,HYPR(KC_E),
                                                            KC_HOME, KC_PGUP,
                                                                     KC_LALT,
-                                         LT(LAYER1,KC_SPC), KC_DEL, S(KC_4),
+                                        LT(LAYER1,KC_SPC), KC_LCTL, S(KC_4),
         // right hand
              KC_GRV,      KC_6,   KC_7,   KC_8,   KC_9,   KC_0,             KC_BSLS,
              KC_LBRC,     KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,             KC_RBRC,
